0bcc2109f724f56bb0979a510e7d303ffca8d5fd
youhaijun <youhaijun@duiba.com.cn>
2018-06-11 14:51:22 +0800

    持久层自动化代码--还有一个bug

--- src/main/java/org/lemon/yhj/generator/freemarker/FreemarkerService.java
+++ src/main/java/org/lemon/yhj/generator/freemarker/FreemarkerService.java
@@ -11,41 +11,41 @@ import freemarker.template.Template;
 import freemarker.template.TemplateException;
 
 /**
- *  FreeMarker模板
+ * FreeMarker模板
  */
 public class FreemarkerService {
 
-	private String location;
+    private String location;
 
-	private String suffix;
+    private String suffix;
 
-	private Configuration cfg = new Configuration();
-	
-	private String encoding = "utf-8";
-	
-	public FreemarkerService(){
-		cfg.setDefaultEncoding(encoding);
-		cfg.setClassForTemplateLoading(FreemarkerService.class, "");
-		cfg.setObjectWrapper(new DefaultObjectWrapper());
-		cfg.setNumberFormat("#");
-		this.location = "";
-		this.suffix = "ftl";
-	}
-	
-	public String merge(String template, Map<String, Object> model) {
-		try {
-			Template tpl = cfg.getTemplate(template+"."+suffix);
-			ByteArrayOutputStream bos = new ByteArrayOutputStream();
-			OutputStreamWriter writer = new OutputStreamWriter(bos);
-			tpl.process(model, writer);
-			writer.flush();
-			writer.close();
-			byte[] byteArray = bos.toByteArray();
-			return new String(byteArray,encoding);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		} catch (TemplateException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    private Configuration cfg = new Configuration();
+
+    private String encoding = "utf-8";
+
+    public FreemarkerService() {
+        cfg.setDefaultEncoding(encoding);
+        cfg.setClassForTemplateLoading(FreemarkerService.class, "");
+        cfg.setObjectWrapper(new DefaultObjectWrapper());
+        cfg.setNumberFormat("#");
+        this.location = "";
+        this.suffix = "ftl";
+    }
+
+    public String merge(String template, Map<String, Object> model) {
+        try {
+            Template tpl = cfg.getTemplate(template + "." + suffix);
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            OutputStreamWriter writer = new OutputStreamWriter(bos);
+            tpl.process(model, writer);
+            writer.flush();
+            writer.close();
+            byte[] byteArray = bos.toByteArray();
+            return new String(byteArray, encoding);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (TemplateException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/test/java/org/lemon/yhj/lemon/CodeGenerator.java
+++ src/test/java/org/lemon/yhj/lemon/CodeGenerator.java
@@ -1,4 +1,4 @@
-package org.lemon.yhj.generator.test;
+package org.lemon.yhj.lemon;
 
 
 import org.lemon.yhj.generator.MybatisGenerator;
@@ -11,25 +11,25 @@ public class CodeGenerator {
 
     public static void main(String[] args) {
         //数据库的连接信息、用户名、密码
-        String DB_URL = "jdbc:mysql://dev.config.duibar.com:3306/tuia_media?createDatabaseIfNotExist=true&useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull&allowMultiQueries=true&connectTimeout=5000&socketTimeout=60000";
-        String user = "dev";
-        String pwd = "dev_fas015";
+        String DB_URL = "jdbc:mysql://47.97.189.222:3309/guide?createDatabaseIfNotExist=true&useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull&allowMultiQueries=true&connectTimeout=5000&socketTimeout=60000";
+        String user = "guide";
+        String pwd = "bxm.iueM^EY!8F";
 
         GeneratorConfig config = new GeneratorConfig(DB_URL, user, pwd);
         //设置生成的 XXXXMapper.java所在的包名
-        config.setDaoPackage("cn.com.duiba.tuia.activity.center.biz.dao.activity");
+        config.setDaoPackage("com.bxm.adsprod.dal.activity.land");
         //设置生成的 XXXXDO.java所在的包名
-        config.setDOpackage("cn.com.duiba.tuia.activity.center.biz.entity");
+        config.setDOpackage("com.bxm.adsprod.dal.activity.land");
         //设置生成的XXX.java存放的模块名
-        config.setModuleName("activity-center-biz");
+        config.setModuleName("lemon");
         //设置生成的XXXXMapper.xml存在的相对路径，相对工程根目录，
-        config.setXmlLocation("activity-center-deploy/src/main/resources/mybatis/activity");
+        config.setXmlLocation("lemon/src/main/resources/mybatis/activity");
         //设置数据库名，库名填错会找不到表
-        config.setTableCatalog("tuia_activity");
+        config.setTableCatalog("guide");
         //设置表名，生成器根据设置的表生成代码
-        config.setTableName("tb_game_robot");
+        config.setTableName("tbl_land_user_info");
         //设置数据表前缀，比如tb_activity的前缀为tb，设置了此项之后生成的do、dao、mapper.xml不包含前缀tb
-        config.setTablePrefix("tb");
+        config.setTablePrefix("tbl");
 
         //代码生成
         MybatisGenerator.genCode(config);
